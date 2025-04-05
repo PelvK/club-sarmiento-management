ec83fab209426e9e7635660798a34a5a48eadb3f
FacuVillalba <109980170+PelvK@users.noreply.github.com>
2025-04-05 20:10:23 -0300

    Update Members.tsx

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -38,7 +38,7 @@ const Members: React.FC = () => {
   return (
     <div>
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-semibold text-gray-900">Socios</h1>
+        <h1 className="text-2xl font-semibold text-gray-900">Socios aa</h1>
         <div className='flex flex-row'>
           <button className="flex items-center mx-4 px-4 py-2 bg-[#FFD700] text-black rounded-md hover:bg-[#FFC000] transition-colors">
             <PlusCircle className="w-5 h-5 mr-2" />
